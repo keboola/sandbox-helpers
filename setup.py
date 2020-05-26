c4c0c3675235416b4bdde276d6dd6c62c5a07589
pivnicek <marc@keboola.com>
2020-05-26 12:39:33 +0200

    add missing files'

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='keboola-sandbox-helpers',
+    version='1.0',
+    url='https://github.com/keboola/sandbox-helpers',
+    packages=['keboola_sandbox_helpers'],
+    package_dir={'keboola_sandbox_helpers': ''},
+    requires=['pip']
+)
